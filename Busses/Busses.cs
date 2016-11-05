93d9a92c96a2da742fa2583fbcb5c18225f7a644
marianbochev <marian_bochev@hotmail.com>
2016-11-05 02:53:26 +0200

    CSharp Preparation for exams

---
+++ Busses/Busses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Busses
+{
+    class Busses
+    {
+        static void Main()
+        {
+            //input
+            int numberOfBusses = int.Parse(Console.ReadLine());
+            int maxSpeed = 1500;
+            int groups = 0;
+            
+            //logic
+            for (int i = 1; i <= numberOfBusses; i++)
+            {
+                int bus = int.Parse(Console.ReadLine());
+                if(bus <= maxSpeed)
+                {
+                    int groupspeed = bus;
+                    maxSpeed = groupspeed;
+                    groups++;
+                    
+                }
+            }
+            Console.WriteLine(groups);
+        }
+    }
+}
---
+++ Pages/Pages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Pages
+{
+    class Pages
+    {
+        static void Main()
+        {
+            //input
+            int digits = int.Parse(Console.ReadLine());
+            int pages = 0;
+            
+            //logic
+            for (int i = 0; digits > 0; i++)
+            {
+                pages++;
+                if(pages < 10)
+                {
+                    digits -= 1;
+                }
+                else if (pages < 100)
+                {
+                    digits -= 2;
+                }
+                else if (pages < 1000)
+                {
+                    digits -= 3;
+                }
+                else if (pages < 10000)
+                {
+                    digits -= 4;
+                }
+                else if (pages < 100000)
+                {
+                    digits -= 5;
+                }
+                else if (pages < 1000000)
+                {
+                    digits -= 6;
+                }
+            }
+            Console.WriteLine(pages);
+        }
+    }
+}
